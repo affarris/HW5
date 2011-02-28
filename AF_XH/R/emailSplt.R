b5d2710647cfdeada95ddb99506e20cb33f509da
Andrew <affarris@ucdavis.edu>
2011-02-27 22:58:04 -0800

    Added functions bodySplt and emailSplt, with documentation

---
+++ AF_XH/R/emailSplt.R
@@ -0,0 +1,15 @@
+
+# takes lists of email header and body indices, and uses them to obtain
+# a list of lists, each with the bodies and headers in them.
+
+emailSplt <- function(linz, headers, bodies){
+    numHds <- length(headers)
+
+    emails <- lapply(1:numHds, function(j, linz, headers, bodies){
+            list(header = linz[ headers[[ j ]] ],
+                body = linz[ bodies[[ j ]] ] )
+                }, linz = linz, 
+                headers = headers,
+                bodies = bodies)
+    return(emails)
+    }
