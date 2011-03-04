963237c26a89601874b337bfe69e8c23e3c404e6
Yi Chen <syichen@ucdavis.edu>
2011-03-04 12:54:02 -0800

    version1.1

--- AF_XH/R/extvalfun.R
+++
@@ -1,9 +0,0 @@
-### extracting the content after a header of one email
-##e.g.,  extvalfun(header) to extract the content after Tag
-
-extvalfun = function(txt){
-          con = textConnection(txt)
-          valuematrix = read.dcf(con)
-          close(con)
-          return(valuematrix)
-}
