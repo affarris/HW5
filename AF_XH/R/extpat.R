4a248534d48879ee1f29ddc771ca6f939440266a
hxyhuhu <xyhu@ucdavis.edu>
2011-03-04 12:58:32 -0800

    updated extpat.r

--- AF_XH/R/extpat.R
+++ AF_XH/R/extpat.R
@@ -1,12 +1,12 @@
  
 ### Extract the interested part of the header ####
 
-extpat=function(list, reexp, el="\\1"){
-    for (i in 1:numFile){
-        for (j in 1: numEmail[i]){
-            tt=list[[i]][j]
-            list[[i]][j]= gsub(reexp,el,tt ) 
+extpat=function(hdlist, reexp, el="\\1"){
+    for (i in 1:length(hdlist)){
+        for (j in 1: length(hdlist[[i]])){
+            tt=hdlist[[i]][j]
+            hdlist[[i]][j]= gsub(reexp,el,tt ) 
                                 }
                     } 
-     return(list)
+     return(hdlist)
       }
